cd7c1ee25f3658a77df8fee4b42312c06bda0ac9
parbilla <parbilla@gmail.com>
2021-05-18 20:45:20 +0000

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,7 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
 def top_ten(subreddit):
     """Returns first 10 hot posts"""
     req = requests.get(url.format(subreddit), headers=headers)
-    if req.status_code == 200:
+    if req.status_code != 404:
         for children in req.json().get('data').get('children'):
             print(children['data']['title'])
     else:
