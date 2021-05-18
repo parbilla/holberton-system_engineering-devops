caf3dac4bd369689eb957feefa62639cff4bc034
parbilla <parbilla@gmail.com>
2021-05-18 20:42:23 +0000

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and  prints the titles
+of the first 10 hot posts listed for a given subreddit."""
+import requests
+
+url = 'https://www.reddit.com/r/{}/top/.json?limit=10'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
+
+
+def top_ten(subreddit):
+    """Returns first 10 hot posts"""
+    req = requests.get(url.format(subreddit), headers=headers)
+    if req.status_code == 200:
+        for children in req.json().get('data').get('children'):
+            print(children['data']['title'])
+    else:
+        print(None)
