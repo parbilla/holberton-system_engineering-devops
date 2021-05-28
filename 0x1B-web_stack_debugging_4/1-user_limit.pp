911a822609a771d28e383d863b99bd0c440eaea6
parbilla <parbilla@gmail.com>
2021-05-28 16:20:42 +0000

    task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
 #Login with user and open file
 exec { 'holbertonUserLimits':
-     command => 'sed -i s/4/20/ /etc/security/limits.conf && sed -i s/5/50/ /etc/security/limits.conf',
-      path   => ['/bin', '/usr/bin', '/usr/sbin']
+      command => 'sed -i s/4/20/ /etc/security/limits.conf && sed -i s/5/50/ /etc/security/limits.conf',
+      path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
